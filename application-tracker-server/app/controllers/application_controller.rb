25f6cecaeb232ebacf01882e4009f6c89bb9406f
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-09-09 16:43:25 -0400

    adds cookie hash method

--- application-tracker-server/app/controllers/application_controller.rb
+++ application-tracker-server/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+
+  def cookie_hash(user)
+    { value: { id: user.id }, expires: 1.day, httponly: true }
+  end
 end
