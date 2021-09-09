7b24dd2edac4ac5028010f1ac7d293f9db11ca67
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-09-09 16:41:29 -0400

    adds signup route

--- application-tracker-server/config/routes.rb
+++ application-tracker-server/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope :api do
+    post '/signup' => 'users#create'
     post '/signin' => 'sessions#create'
     delete '/signout' => 'sessions#destroy'
     get '/current' => 'sessions#show'
