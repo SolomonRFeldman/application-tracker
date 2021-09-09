7b1f8c6ad6252222c430efcf43fe9896b74e036d
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-09-09 16:35:08 -0400

    changes username validation to match db column

--- application-tracker-server/app/models/user.rb
+++ application-tracker-server/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, precence: true
+  validates :username, precence: true
 
   validates :email, precence: true
   validates :email, uniqueness: { case_sensative: false }
