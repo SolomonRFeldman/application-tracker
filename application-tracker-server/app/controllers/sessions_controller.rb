7e617d1a3f7b247246180a21a175940e61654737
SolomonRFeldman <SolomonRFeldman@gmail.com>
2021-09-09 14:18:28 -0400

    adds basic cookie handling

---
+++ application-tracker-server/app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def create
+    cookies.signed[:user] = { value: { name: params[:user][:email] }, httponly: true }
+    # cookies.delete :user
+    puts cookies.signed[:user]
+    render json: { user: cookies.signed[:user] }
+  end
+
+  def destroy
+    puts cookies.delete(:user)
+    render status: 200
+  end
+
+  def show
+    render json: { user: cookies.signed[:user] }
+  end
+
+end
