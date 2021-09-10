8e7b78c5f93796d445ad5983d2c75df4ba4852e0
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-09-10 09:23:21 -0400

    makes cookie_hash a private method

--- application-tracker-server/app/controllers/application_controller.rb
+++ application-tracker-server/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  private
+
   def cookie_hash(user)
     { value: { id: user.id }, expires: 1.day, httponly: true }
   end
