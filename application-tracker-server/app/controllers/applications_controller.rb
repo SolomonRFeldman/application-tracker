ad1a60297ca1944f68e292a2b6cc97bd42df2c16
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-09-17 15:11:19 -0400

    fixes double json conversion on create

--- application-tracker-server/app/controllers/applications_controller.rb
+++ application-tracker-server/app/controllers/applications_controller.rb
@@ -9,7 +9,7 @@ class ApplicationsController < ApplicationController
   def create
     if @current_user_id == params[:user_id].to_i
       application = Application.new(application_params.merge({user_id: params[:user_id]}))
-      application.save ? render(json: application.to_json, status: 200) : render(json: { errors: application.errors }, status: 400)
+      application.save ? render(json: application, status: 200) : render(json: { errors: application.errors }, status: 400)
     end
   end
 
