af5032840a9f01786689e07af041a40d414fc436
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-10-06 14:38:26 -0400

    adds email uniqueness test

--- application-tracker-server/spec/models/user_spec.rb
+++ application-tracker-server/spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:valid_user) { create(:valid_user) }
+  let(:valid_user2) { create(:valid_user2) }
 
   it 'is valid with a username, email, and password' do
     expect(valid_user).to be_valid
@@ -41,6 +42,20 @@ RSpec.describe User, type: :model do
 
   it 'is invalid with a blank password' do
     expect(build(:valid_user, password: '')).to_not be_valid
+  end
+
+  context 'when another user has taken an email' do
+    before do
+      valid_user
+    end
+
+    it 'is invalid with the same email as the other user' do
+      expect(build(:valid_user2, email: 'Test@123.com')).to_not be_valid
+    end
+
+    it 'is invalid with the same email as the other user in a different case' do
+      expect(build(:valid_user2, email: 'test@123.com')).to_not be_valid
+    end
   end
 
 end
