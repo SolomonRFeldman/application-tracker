db5c0772e40a1d454fa24523c5a748b939964ef2
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-10-06 17:12:20 -0400

    adds presence check to status

--- application-tracker-server/app/models/application.rb
+++ application-tracker-server/app/models/application.rb
@@ -3,6 +3,7 @@ class Application < ApplicationRecord
 
   enum status: {accepted: 'accepted', declined: 'declined', pending: 'pending'}
 
+  validates :status, presence: true
   validates :organization_name, presence: true
   validates :purpose, presence: true
 end
